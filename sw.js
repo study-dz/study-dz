4c4ae38cb86c08d3b92e0d479dac489f86490c06
study-dz <ishak08bm@gmail.com>
2026-09-22 23:32:26 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='study-dz-century-v1';
+const CORE=['./','./index.html','./style.css','./js/app.js','./js/users.js','./js/quran.js','./js/century.js','./manifest.webmanifest','./assets/audio/001.mp3'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return x}).catch(()=>caches.match('./index.html'))));});
